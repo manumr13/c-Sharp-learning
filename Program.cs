4cd7c30c935082e41b3d2a0b92c85f405fce96e3
manumr13 <manumr8296@gmail.com>
2024-04-30 13:17:29 +0530

    dding

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+//Tom Jefferson is 19 years old, his weight is 50 kg and his height is 156.5 cm.
+namespace namu{
+    class first{
+        static void Main(string[] args){
+        Console.WriteLine("How many people do you want to calculate the bmi ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        for (int i=1;i<=x;i++){
+                Console.WriteLine("Enter the person details enter kg first and meter second");
+                int kg = Convert.ToInt32(Console.ReadLine());
+                double meter = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine(kg/Math.Pow(meter,2));
+            }
+            Console.WriteLine("Thank you so much");
+        }
+    }
+}
